6022f0b9f50d49c052777981757e5ec52a2c9f1c
Dylan McDonald <dmcdonald21@cnm.edu>
2016-12-04 00:30:33 -0700

    added mutator methods

--- php/classes/Rsvp.php
+++ php/classes/Rsvp.php
@@ -55,12 +55,52 @@ class Rsvp implements \JsonSerializable {
 	}
 
 	/**
+	 * mutator method for rsvp id
+	 *
+	 * @param int|null $newRsvpId new value of rsvp id
+	 * @throws \RangeException if $newRsvpId is not positive
+	 * @throws \TypeError if $newRsvpId is not an integer
+	 **/
+	public function setRsvpId(int $newRsvpId = null) {
+		// base case: if the rsvp id is null, this a new rsvp without a mySQL assigned id (yet)
+		if($newRsvpId === null) {
+			$this->rsvpId = null;
+			return;
+		}
+
+		// verify the rsvp id is positive
+		if($newRsvpId <= 0) {
+			throw(new \RangeException("rsvp id is not positive"));
+		}
+
+		// convert and store the rsvp id
+		$this->rsvpId = $newRsvpId;
+	}
+
+	/**
 	 * accessor method for invitee id
 	 *
 	 * @return int value of invitee id
 	 **/
 	public function getRsvpInviteeId() {
 		return($this->rsvpInviteeId);
+	}
+
+	/**
+	 * mutator method for rsvp invitee id
+	 *
+	 * @param int $newRsvpInviteeId new value of rsvp invitee id
+	 * @throws \RangeException if $newRsvpInviteeId is not positive
+	 * @throws \TypeError if $newRsvpInviteeId is not an integer
+	 **/
+	public function setRsvpInviteeId(int $newRsvpInviteeId) {
+		// verify the rsvp invitee id is positive
+		if($newRsvpInviteeId <= 0) {
+			throw(new \RangeException("rsvp invitee id is not positive"));
+		}
+
+		// convert and store the rsvp id
+		$this->rsvpInviteeId = $newRsvpInviteeId;
 	}
 
 	/**
@@ -73,12 +113,60 @@ class Rsvp implements \JsonSerializable {
 	}
 
 	/**
+	 * mutator method for rsvp comment
+	 *
+	 * @param string|null $newRsvpComment new value of rsvp comment
+	 * @throws \InvalidArgumentException if $newRsvpComment is not a string or insecure
+	 * @throws \RangeException if $newRsvpComment is > 128 characters
+	 * @throws \TypeError if $newRsvpComment is not a string
+	 **/
+	public function setRsvpComment(string $newRsvpComment = null) {
+		// base case: if the rsvp comment is null, allow it to be null
+		if($newRsvpComment === null) {
+			$this->rsvpComment = null;
+			return;
+		}
+
+		// verify the rsvp comment is secure
+		$newRsvpComment = trim($newRsvpComment);
+		$newRsvpComment = filter_var($newRsvpComment, FILTER_SANITIZE_EMAIL);
+		if(empty($newRsvpComment) === true) {
+			throw(new \InvalidArgumentException("rsvp comment is empty or insecure"));
+		}
+
+		// verify the rsvp comment will fit in the database
+		if(strlen($newRsvpComment) > 128) {
+			throw(new \RangeException("rsvp comment too large"));
+		}
+
+		// store the rsvp comment
+		$this->rsvpComment = $newRsvpComment;
+	}
+
+	/**
 	 * accessor method for rsvp IP address
 	 *
 	 * @return string value of rsvp IP address in printable form
 	 **/
 	public function getRsvpIpAddress() {
 		return(inet_ntop($this->rsvpIpAddress));
+	}
+
+	/**
+	 * Mutator for rsvp IP address
+	 *
+	 * @param string $newRsvpIpAddress new value of rsvp IP address
+	 * @throws \InvalidArgumentException if $newRsvpIpAddress is not a valid IP address
+	 **/
+	public function setRsvpIpAddress(string $newRsvpIpAddress) {
+		// detect the IP's format and assign it in binary mode
+		if(@inet_pton($newRsvpIpAddress) !== false) {
+			$this->rsvpIpAddress = inet_pton($newRsvpIpAddress);
+		} else if(@inet_ntop($ipAddress) !== false) {
+			$this->rsvpIpAddress = $newRsvpIpAddress;
+		} else {
+			throw(new InvalidArgumentException("invalid rsvp IP address"));
+		}
 	}
 
 	/**
@@ -91,11 +179,53 @@ class Rsvp implements \JsonSerializable {
 	}
 
 	/**
+	 * mutator method for rsvp number of people
+	 *
+	 * @param int $newRsvpNumPeople new value of rsvp number of people
+	 * @throws \RangeException if $newRsvpNumPeople is negative
+	 * @throws \TypeError if $newRsvpNumPeople is not an integer
+	 **/
+	public function setRsvpNumPeople(int $newRsvpNumPeople) {
+		// verify the number of people is not negative
+		if($newRsvpNumPeople < 0) {
+			throw(new \RangeException("rsvp number of people is cannot be negative"));
+		}
+
+		// convert and store the number of people
+		$this->rsvpNumPeople = $newRsvpNumPeople;
+	}
+
+	/**
 	 * accessor method for rsvp timestamp
 	 *
 	 * @return \DateTime value of rsvp timestamp
 	 **/
 	public function getRsvpTimestamp() {
 		return($this->rsvpTimestamp);
+	}
+
+	/**
+	 * mutator method for rsvp timestamp
+	 *
+	 * @param \DateTime|string|null $newRsvpTimestamp rsvp timestamp as a DateTime object or string (or null to load the current time)
+	 * @throws \InvalidArgumentException if $newRsvpTimestamp is not a valid object or string
+	 * @throws \RangeException if $newRsvpTimestamp is a date that does not exist
+	 **/
+	public function setRsvpTimestamp($newRsvpTimestamp = null) {
+		// base case: if the rsvp timestamp is null, use the current date and time
+		if($newRsvpTimestamp === null) {
+			$this->rsvpTimestamp = new \DateTime();
+			return;
+		}
+
+		// store the rsvp timestamp
+		try {
+			$newRsvpTimestamp = self::validateDateTime($newRsvpTimestamp);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}
+		$this->rsvpTimestamp = $newRsvpTimestamp;
 	}
 }
