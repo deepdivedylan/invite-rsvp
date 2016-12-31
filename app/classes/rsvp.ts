a0f6df63705a1707575c6378194eb116221f5a01
Dylan McDonald <dmcdonald21@cnm.edu>
2016-12-30 18:31:06 -0700

    added RSVP class

---
+++ app/classes/rsvp.ts
@@ -0,0 +1,3 @@
+export class Rsvp {
+	constructor(public rsvpId: number, public rsvpInviteeId: number, public rsvpBrowser: string, public rsvpComment: string, public rsvpIpAddress: string, public rsvpNumPeople: number, public rsvpTimestamp: Date) {}
+}
