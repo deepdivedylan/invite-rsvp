a4cd890ffc3ece175b46e3ac644f28292fa5622e
Dylan McDonald <dmcdonald21@cnm.edu>
2016-12-30 21:05:24 -0700

    modified splash to redirect to login

--- app/components/splash-component.ts
+++ app/components/splash-component.ts
@@ -1,7 +1,24 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {LoginStatus} from "../classes/login-status";
+import {LoginService} from "../services/login-service";
 
 @Component({
 	templateUrl: "./templates/splash.php"
 })
 
-export class SplashComponent {}
+export class SplashComponent implements OnInit {
+	private loginStatus : LoginStatus = new LoginStatus(false);
+
+	constructor(private loginService: LoginService, private router: Router) {}
+
+	ngOnInit() : void {
+		this.loginService.isLoggedIn()
+			.subscribe(loginStatus => {
+				this.loginStatus = loginStatus;
+				if(this.loginStatus.loginStatus === false) {
+					this.router.navigate(["/login"]);
+				}
+			});
+	}
+}
