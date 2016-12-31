a6a1f664cb0d239371a4c179997ab51a446c32de
Dylan McDonald <dmcdonald21@cnm.edu>
2016-12-30 20:54:25 -0700

    added method to determine whether logged in

--- app/services/login-service.ts
+++ app/services/login-service.ts
@@ -3,6 +3,7 @@ import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import {BaseService} from "./base-service";
 import {Login} from "../classes/login";
+import {LoginStatus} from "../classes/login-status";
 import {Status} from "../classes/status";
 
 @Injectable()
@@ -16,6 +17,12 @@ export class LoginService extends BaseService {
 	login(login: Login) : Observable<Status> {
 		return(this.http.post(this.loginUrl, login)
 			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+
+	isLoggedIn() : Observable<LoginStatus> {
+		return(this.http.get(this.loginUrl)
+			.map(this.extractData)
 			.catch(this.handleError));
 	}
 }
