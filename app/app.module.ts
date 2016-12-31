5d77eeb591626c55106aa595f4def15a65f75a52
Dylan McDonald <dmcdonald21@cnm.edu>
2016-12-30 19:13:33 -0700

    added services to app module

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,9 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
+import {InviteeService} from "./services/invitee-service";
+import {LoginService} from "./services/login-service";
+import {RsvpService} from "./services/rsvp-service";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 
 const moduleDeclarations = [AppComponent];
@@ -11,6 +14,6 @@ const moduleDeclarations = [AppComponent];
 	imports:      [BrowserModule, FormsModule, HttpModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
-	providers:    [appRoutingProviders]
+	providers:    [appRoutingProviders, InviteeService, LoginService, RsvpService]
 })
 export class AppModule {}
