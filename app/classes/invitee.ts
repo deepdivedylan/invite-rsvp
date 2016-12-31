d79052c09918a7418b57f5c047eb99f762384dcd
Dylan McDonald <dmcdonald21@cnm.edu>
2016-12-30 18:24:43 -0700

    added invitee class

---
+++ app/classes/invitee.ts
@@ -0,0 +1,3 @@
+export class Invitee {
+	constructor(public inviteeId: number, public inviteeCity: string, public inviteeCountry: string, public inviteeEmail: string, public inviteePhone: string, public inviteeState: string, public inviteeToken: string, public inviteeZip: string) {}
+}
