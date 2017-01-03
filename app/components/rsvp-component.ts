5d24b9dbebe7180d7d8492cb8e6f75d93837ae39
Dylan McDonald <dmcdonald21@cnm.edu>
2017-01-02 21:44:06 -0700

    added starter RSVP component and template

--- app/components/rsvp-component.ts
+++ app/components/rsvp-component.ts
@@ -1,18 +1,28 @@
 import {Component, OnInit} from "@angular/core";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params} from "@angular/router";
 import {Invitee} from "../classes/invitee";
 import {InviteeService} from "../services/invitee-service";
+import {Status} from "../classes/status";
+import "rxjs/add/operator/switchMap";
 
 @Component({
 	templateUrl: "./templates/rsvp.php"
 })
 
 export class RsvpComponent implements OnInit {
+	invitee : Invitee = null;
+	status : Status = null;
 
-
-	constructor(private inviteeService: InviteeService, private router: Router) {}
+	constructor(private inviteeService: InviteeService, private route: ActivatedRoute) {}
 
 	ngOnInit() : void {
-		;
+		this.getInviteeByToken();
 	}
+
+	getInviteeByToken() : void {
+		this.route.params
+			.switchMap((params : Params) => this.inviteeService.getInviteeByInviteeToken(params["inviteeToken"]))
+			.subscribe(invitee => this.invitee = invitee);
+	}
+
 }
--- public_html/templates/rsvp.php
+++ public_html/templates/rsvp.php
@@ -1,1 +1,14 @@
-<h1>RSVP Component</h1>
+<div *ngIf="invitee === undefined">
+	<h1>Invitee Not Found</h1>
+	<div class="alert alert-danger" role="alert">
+		Invitee not found. Please verify the link and try again.
+	</div>
+</div>
+<div *ngIf="invitee !== undefined && invitee !== null">
+	<h1>RSVP For {{ invitee.inviteeName }}</h1>
+	<pre>{{ invitee | json }}</pre>
+	<div *ngIf="status !== null" class="alert alert-dismissible" [ngClass]="status.type" role="alert">
+		<button type="button" class="close" aria-label="Close" (click)="status = null;"><span aria-hidden="true">&times;</span></button>
+		{{ status.message }}
+	</div>
+</div>
