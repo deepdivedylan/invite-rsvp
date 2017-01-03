1e922230bfcb5176ac1271131a4eb035e533cc65
Dylan McDonald <dmcdonald21@cnm.edu>
2017-01-02 19:50:06 -0700

    added loading of invitees

--- app/components/splash-component.ts
+++ app/components/splash-component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
+import {Invitee} from "../classes/invitee";
+import {InviteeService} from "../services/invitee-service";
 import {LoginStatus} from "../classes/login-status";
 import {LoginService} from "../services/login-service";
 
@@ -9,8 +11,9 @@ import {LoginService} from "../services/login-service";
 
 export class SplashComponent implements OnInit {
 	private loginStatus : LoginStatus = new LoginStatus(false);
+	private invitees : Invitee[] = [];
 
-	constructor(private loginService: LoginService, private router: Router) {}
+	constructor(private inviteeService: InviteeService, private loginService: LoginService, private router: Router) {}
 
 	ngOnInit() : void {
 		this.loginService.isLoggedIn()
@@ -18,7 +21,14 @@ export class SplashComponent implements OnInit {
 				this.loginStatus = loginStatus;
 				if(this.loginStatus.loginStatus === false) {
 					this.router.navigate(["/login"]);
+				} else {
+					this.getAllInvitees();
 				}
 			});
+	}
+
+	getAllInvitees() : void {
+		this.inviteeService.getAllInvitees()
+			.subscribe(invitees => this.invitees = invitees);
 	}
 }
--- public_html/templates/splash.php
+++ public_html/templates/splash.php
@@ -1,1 +1,26 @@
-<h1>Splash Component</h1>
+<h1>Invite &amp; RSVP Admin</h1>
+<h2>Invitees</h2>
+<table class="table table-bordered table-responsive table-striped table-word-wrap">
+	<tr>
+		<th>Invitee ID</th>
+		<th>Invitee Name</th>
+		<th>Invitee Email</th>
+		<th>Invitee Phone</th>
+		<th>Invitee Street</th>
+		<th>Invitee Street 2</th>
+		<th>Invitee State</th>
+		<th>Invitee ZIP</th>
+		<th>Invitee Country</th>
+	</tr>
+	<tr *ngFor="let invitee of invitees">
+		<td>{{ invitee.inviteeId }}</td>
+		<td>{{ invitee.inviteeName }}</td>
+		<td><a [href]="'mailto:' + invitee.inviteeEmail">{{ invitee.inviteeEmail }}</a></td>
+		<td><a [href]="'tel:' + invitee.inviteePhone">{{ invitee.inviteePhone }}</a></td>
+		<td>{{ invitee.inviteeStreet1 }}</td>
+		<td>{{ invitee.inviteeStreet2 }}</td>
+		<td>{{ invitee.inviteeState }}</td>
+		<td>{{ invitee.inviteeZip }}</td>
+		<td>{{ invitee.inviteeCountry }}</td>
+	</tr>
+</table>
