9423a3734db9c33d0829527cba1ac289bec7540a
Dylan McDonald <dmcdonald21@cnm.edu>
2017-01-04 21:26:18 -0700

    added street address to class

--- app/classes/invitee.ts
+++ app/classes/invitee.ts
@@ -1,3 +1,3 @@
 export class Invitee {
-	constructor(public inviteeId: number, public inviteeCity: string, public inviteeCountry: string, public inviteeEmail: string, public inviteePhone: string, public inviteeState: string, public inviteeToken: string, public inviteeZip: string) {}
+	constructor(public inviteeId: number, public inviteeCity: string, public inviteeCountry: string, public inviteeEmail: string, public inviteePhone: string, public inviteeState: string, public inviteeStreet1: string, public inviteeStreet2 : string, public inviteeToken: string, public inviteeZip: string) {}
 }
